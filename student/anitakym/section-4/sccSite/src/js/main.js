5b0ce0f5f627cf99512fc25ab47388db45129cf0
anitakym <git@kuangyimin56@gmail.com>
2016-12-29 18:30:46 +0800

    sccsite

---
+++ student/anitakym/section-4/sccSite/src/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kym on 2016/11/26.
+ */
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPAuse:true,
+
+
+    });
+});
