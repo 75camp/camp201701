2b108b8a5f5d096a269e571efe3ef9a11c576d4a
liuliangsir <2269305724@qq.com>
2017-02-23 00:06:41 +0800

    :bug:修复逻辑错误

--- student/liuliang/task/20161231/javascript-practice/javascript-practice-1.js
+++ student/liuliang/task/20161231/javascript-practice/javascript-practice-1.js
@@ -1,20 +1,21 @@
 ;(function(win){
   var shoppingCardFormatAutoCorrect = function(str){
+    var regexp = /-/g
     var throwErrors = function(message = '无效的密码格式') {
       try{
         throw new Error(message)
       } catch(ex) {
-        console.debug(ex)
+        console.debug(ex.message)
         return false
       }
     }
     var isShoppingCardStringLengthLegal = function(str,length = 20) {
-      return str.length === length ? true : false
+      return str.length === length
     }
-    var isShoppingCardFormatLegal = function(str, regexp = /^(?!.*(\s|\?))[a-zA-Z0-9]*-+/) {
+    var isShoppingCardFormatLegal = function(str, regexp = /^(?!.*(\s|\?))[a-zA-Z0-9]*/) {
       return regexp.test(str)
     }
-    str = str.replace(regexp).toUpperCase()
+    str = str.replace(regexp, '').toUpperCase()
 
     switch (false) {
       case isShoppingCardStringLengthLegal(str): return throwErrors('数据格式有误') && false
