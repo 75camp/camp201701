2d78b6606ffdaf3458a0c96099a491c42e957fa9
liuliangsir <2269305724@qq.com>
2017-02-22 20:43:18 +0800

    :hammer:重构代码

--- student/liuliang/task/20161231/javascript-practice/javascript-practice-1.js
+++ student/liuliang/task/20161231/javascript-practice/javascript-practice-1.js
@@ -1,9 +1,14 @@
 ;(function(win){
-  var shoppingCardFormatAutoCorrect ＝ function(str){}
+  var shoppingCardFormatAutoCorrect = function(str){
     var throwErrors = function(message = '无效的密码格式') {
-      return throw new Error(message) && false
+      try{
+        throw new Error(message)
+      } catch(ex) {
+        console.debug(ex)
+        return false
+      }
     }
-    var isStringLengthLegal = function(str,length = 20) {
+    var isShoppingCardStringLengthLegal = function(str,length = 20) {
       return str.length === length ? true : false
     }
     var isShoppingCardFormatLegal = function(str, regexp = /^(?!.*(\s|\?))[a-zA-Z0-9]*-+/) {
@@ -12,7 +17,7 @@
     str = str.replace(regexp).toUpperCase()
 
     switch (false) {
-      case isStringLengthLegal(str): return throwErrors('数据格式有误') && false
+      case isShoppingCardStringLengthLegal(str): return throwErrors('数据格式有误') && false
       case isShoppingCardFormatLegal(str): return throwErrors('密码格式无效') && false
       default:
         return str
