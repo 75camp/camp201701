05798ac7bd53cc53dbed53bdd07b6ef12f58e8f9
liuliangsir <2269305724@qq.com>
2017-03-26 11:49:13 +0800

    :sparkles:增加自己对操作DOM的思考

--- student/liuliang/task/20161231/reflect/problem-1/js/index.js
+++ student/liuliang/task/20161231/reflect/problem-1/js/index.js
@@ -3,7 +3,7 @@
  */
 (function(win, doc){
   var list = doc.querySelector('#user-list');
-  var items = list.querySelectorAll('li'); // static NodeList
+  var items = list.querySelectorAll('li > a'); // static NodeList
   // var child = list.childNodes; // live NodeList contain text node
   // var child = list.children; // HTMLCollection not contain text node
 
@@ -12,13 +12,19 @@
     var resetAll = function(){
       // items NodeList类型(https://developer.mozilla.org/en-US/docs/Web/API/NodeList)，自带item方法，新增forEach、keys、values、entries方法
       items.forEach(function(item){
-        var itemClassList = item.classList; //add、remove、item、toggle、contains
-        if(itemClassList.contains('active')) itemClassList.remove('active');
+        // var itemClassList = item.classList; //add、remove、item、toggle、contains
+        // if(itemClassList.contains('active')) itemClassList.remove('active');
+        item.href = 'javascript:;';
+        // console.log(item.)
       });
     };
     var givenItemOperate = function(){
       var current = event.target;
-      current.classList.toggle('active')
+      var dataSet = current.dataset;
+      // console.log(dataSet)
+      // current.classList.toggle('active')
+      // current.href = '#'+dataSet.id;
+      current.setAttribute('href','#'+dataSet.id)
     };
     var initial = function(){
       resetAll();
